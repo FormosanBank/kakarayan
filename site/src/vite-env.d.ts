b8080333da1864bb0ab335a506926730f06f6c6d
Hunter S <67876717+hunterschep@users.noreply.github.com>
2026-08-20 00:12:19 -0700

    Add Lightsail query API deployment

--- site/src/vite-env.d.ts
+++ site/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_LIVE_API_URL?: string;
+  readonly VITE_KAKARAYAN_API_URL?: string;
   readonly VITE_APP_VERSION?: string;
 }
 
